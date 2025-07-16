d8afef66517e1277aa6f9b7027ebf3457fa53215
Dadmehr <134191240+BDadmehr0@users.noreply.github.com>
2025-07-16 04:28:45 +0330

    Update and rename README.mkd to README.mk

--- README.mk
+++ README.mk
@@ -1,20 +1,28 @@
-# waybackurls
+# Waybackurls with Python
 
 Accept line-delimited domains on stdin, fetch known URLs from the Wayback Machine for `*.domain` and output them on stdout.
 
-Usage example:
+## Basic Usage
 
-```
-▶ cat domains.txt | waybackurls > urls
+### Single Domain
+
+``` sh
+python script.py example.com
 ```
 
-Install:
+### Multiple Domains (via stdin)
 
+``` sh
+cat domains.txt | python main.py
 ```
-▶ go install github.com/tomnomnom/waybackurls@latest
+or
+
+``` sh
+echo "example.com" | python main.py
 ```
 
 ## Credit
 
+forked from [tomnomnom/waybackurls](https://github.com/tomnomnom/waybackurls)
 This tool was inspired by @mhmdiaa's [waybackurls.py](https://gist.github.com/mhmdiaa/adf6bff70142e5091792841d4b372050) script.
 Thanks to them for the great idea!
